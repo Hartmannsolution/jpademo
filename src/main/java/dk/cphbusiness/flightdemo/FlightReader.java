482b6fa84e6ad76bdba237032b3d8b8dc701c044
Thomas Hartmann <thomas@webtrade.dk>
2024-08-16 10:58:20 +0200

    Added stream example to FlightReader

--- src/main/java/dk/cphbusiness/flightdemo/FlightReader.java
+++ src/main/java/dk/cphbusiness/flightdemo/FlightReader.java
@@ -15,17 +15,16 @@ import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
 import dk.cphbusiness.persistence.jdbc.Demo;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Purpose:
@@ -33,63 +32,99 @@ import java.util.Scanner;
  * @author: Thomas Hartmann
  */
 public class FlightReader {
-    private static ObjectMapper objectMapper = new ObjectMapper();
-
     private final String FLIGHT_URL = "https://api.aviationstack.com/v1/flights?access_key=%s&limit=%s&offset=%s";
-    // jackson objectmapper
-
-    public List<FlightDTO> reader(String urlString,  int numberOfRequests, int limit) throws IOException{
+    public List<FlightDTO> reader(String urlString, int numberOfRequests, int limit) throws IOException {
         List<FlightDTO> flightDTOList = new ArrayList();
-        Properties props = new Properties();
-        props.load(Demo.class.getClassLoader().getResourceAsStream("config.properties"));
-        int offset = 0;
-        FlightCollectionDTO flights = null;
         List<FlightCollectionDTO> flightCollectionDTOList = new ArrayList<>();
 
-        for(int i = 0; i < numberOfRequests; i++){
-            urlString = String.format(urlString,  props.getProperty("aviation.key"), limit, offset );
-            String jsonStr = "";
+        int offset = 0;
+        FlightCollectionDTO flights = null;
+
+        for (int i = 0; i < numberOfRequests; i++) {
+            urlString = String.format(urlString, getPropertyValue("aviation.key"), limit, offset);
             URL url = new URL(urlString);
-            flights = objectMapper.readValue(url, FlightCollectionDTO.class );
+            flights = getObjectMapper()
+                    .readValue(url, FlightCollectionDTO.class);
             flightCollectionDTOList.add(flights);
             offset += limit;
         }
-        System.out.println("Finished reading\n"+flightCollectionDTOList);
+
         flightCollectionDTOList.forEach(flightCollectionDTO -> {
-            for(FlightDTO flightDTO : flightCollectionDTO.getData()){
+            for (FlightDTO flightDTO : flightCollectionDTO.getData()) {
                 flightDTOList.add(flightDTO);
             }
         });
         return flightDTOList;
     }
+
     public void jsonToFile(List<FlightDTO> flightCollection, String fileName) throws IOException {
-        objectMapper.writeValue(Paths.get(fileName).toFile(), flightCollection);
+        getObjectMapper().writeValue(Paths.get(fileName).toFile(), flightCollection);
     }
 
     public List<FlightDTO> jsonFromFile(String fileName) throws IOException {
-        List<FlightDTO> flights = objectMapper.readValue(Paths.get(fileName).toFile(), List.class);
+        List<FlightDTO> flights = getObjectMapper().readValue(Paths.get(fileName).toFile(), List.class);
         return flights;
     }
 
-    public static void main(String[] args) {
+    private static ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.writer(new DefaultPrettyPrinter());
+        return objectMapper;
+    }
+    public static void main(String[] args) {
         FlightReader flightReader = new FlightReader();
         try {
-//            List<FlightDTO> result = flightReader.reader(flightReader.FLIGHT_URL, 50, 100);
-//            flightReader.jsonToFile(result, "flightsfile.json");
-//            System.out.println(result);
-//
-            // read FlightCollectionDTO from file
-            List<FlightDTO> flights = objectMapper.readValue(Paths.get("flights.json").toFile(), List.class);
-            System.out.println("FlIGHT COLLECTION: "+flights);
-
+            List<FlightDTO> flightList = getFlightsFromFile();
+            List<FlightInfo> flightInfoList = getFlightInfoDetails(flightList);
+            flightInfoList.forEach(f->{
+                System.out.println(f);
+                System.out.println();
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static List<FlightInfo> getFlightInfoDetails(List<FlightDTO> flightList) {
+        List<FlightInfo> flightInfoList = flightList.stream().map(flight -> {
+            Duration duration = Duration.between(flight.getDeparture().getScheduled(), flight.getArrival().getScheduled());
+            FlightInfo flightInfo = FlightInfo.builder()
+                    .name(flight.getFlight().getNumber())
+                    .iata(flight.getFlight().getIata())
+                    .airline(flight.getAirline().getName())
+                    .duration(duration)
+                    .departure(flight.getDeparture().getScheduled().toLocalDateTime())
+                    .arrival(flight.getArrival().getScheduled().toLocalDateTime())
+                    .origin(flight.getDeparture().getAirport())
+                    .destination(flight.getArrival().getAirport())
+                    .build();
+
+            return flightInfo;
+        }).toList();
+        return flightInfoList;
+    }
+
+    private static List<FlightDTO> getFlightsFromFile() throws IOException {
+        FlightDTO[] flights = getObjectMapper().readValue(Paths.get("flights.json").toFile(), FlightDTO[].class);
+        System.out.println("FlIGHT COLLECTION: " + flights);
+
+        List<FlightDTO> flightList = Arrays.stream(flights).toList();
+        return flightList;
+    }
+
+    private static void writeFlightsToFile(FlightReader flightReader) throws IOException {
+        List<FlightDTO> result = flightReader.reader(flightReader.FLIGHT_URL, 50, 100);
+        flightReader.jsonToFile(result, "flightsfile.json");
+        System.out.println(result);
+    }
+
+    private String getPropertyValue(String key) throws IOException {
+        Properties props = new Properties();
+        props.load(Demo.class.getClassLoader().getResourceAsStream("config.properties"));
+        return props.getProperty(key);
+    }
     @Getter
     @Setter
     @NoArgsConstructor
@@ -97,7 +132,7 @@ public class FlightReader {
     @AllArgsConstructor
     @Builder
     @EqualsAndHashCode
-    private static class FlightCollectionDTO{
+    private static class FlightCollectionDTO {
         private FlightDTO[] data;
     }
 
@@ -108,7 +143,7 @@ public class FlightReader {
     @AllArgsConstructor
     @Builder
     @EqualsAndHashCode
-    private static class FlightDTO{
+    private static class FlightDTO {
         private String flight_date;
         private String flight_status;
         private AirportTime departure;
@@ -124,12 +159,10 @@ public class FlightReader {
     @AllArgsConstructor
     @Builder
     @EqualsAndHashCode
-    private static class AirportTime{
+    private static class AirportTime {
         private String airport;
         private String timezone;
         private String iata;
-//        @JsonSerialize(using = OffsetDateTimeSerializer.class)
-//        @JsonDeserialize(using = OffsetTimeDeserializer.class)
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
         private OffsetDateTime scheduled;
     }
@@ -142,7 +175,7 @@ public class FlightReader {
     @AllArgsConstructor
     @Builder
     @EqualsAndHashCode
-    private static class AirlineDTO{
+    private static class AirlineDTO {
         private String name;
         private String iata;
     }
@@ -154,8 +187,26 @@ public class FlightReader {
     @AllArgsConstructor
     @Builder
     @EqualsAndHashCode
-    private static class AirplaneDTO{
+    private static class AirplaneDTO {
         private String number;
         private String iata;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @ToString
+    @AllArgsConstructor
+    @Builder
+    @EqualsAndHashCode
+    private static class FlightInfo {
+        private String name;
+        private String iata;
+        private String airline;
+        private Duration duration;
+        private LocalDateTime departure;
+        private LocalDateTime arrival;
+        private String origin;
+        private String destination;
     }
 }
